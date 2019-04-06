da49ae0549d7e2932d1720aab3dfc93f0775ce11
JeromeHD <jaymagolan@gmail.com>
2019-04-06 17:06:20 -0400

    heroku

--- server.js
+++ server.js
@@ -17,9 +17,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", {
-  useNewUrlParser: true
-});
+// mongoose.connect("mongodb://localhost/mongoHeadlines", {
+//   useNewUrlParser: true
+// });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
