7c1e1fead1a708fa3460cc225d435fcc2da0fce5
JeromeHD <jaymagolan@gmail.com>
2019-04-06 18:35:22 -0400

    html css

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var path = require("path");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -11,7 +13,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -20,53 +22,72 @@ var app = express();
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", {
-  useNewUrlParser: true
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
 });
 
 // Routes
 
-// A GET route for scraping the echoJS website
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/saved", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/savedArticles.html"));
+});
+
+// A GET route for scraping the echojs website
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  // First, we grab the body of the html with request
+  axios.get("https://www.nytimes.com/section/technology").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+
+    let counter = 0;
+    // var dataArr = [];
+    $("article").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      var storyDiv = $(this).children("div.story-body");
+      result.url = storyDiv.children("a").attr("href");
+      var metaDiv = storyDiv.children("a").children("div.story-meta");
+      result.headline = metaDiv.children("h2").text();
+      result.summary = metaDiv.children("p.summary").text();
 
       // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
+      if (result.headline && result.url) {
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+            counter++;
+            // dataArr.push(dbArticle)
+            console.log("added " + counter + " new items");
+          })
+          .catch(function(err) {
+            // If an error occurred, send it to the client
+            return res.json(err);
+          });
+        // console.log(result)
+        // console.log("added " + incr + " new items")
+      }
     });
 
-    // Send a message to the client
-    res.send("Scrape Complete");
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.sendFile(path.join(__dirname, "public/index.html"));
   });
 });
 
@@ -100,6 +121,21 @@ app.get("/articles/:id", function(req, res) {
     });
 });
 
+// Route for grabbing a specific Article by id, and update it's isSaved property
+app.put("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that updates the matching one in our db...
+  db.Article.update({ _id: req.params.id }, { $set: { isSaved: true } })
+
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
   // Create a new note and pass the req.body to the entry
@@ -112,6 +148,21 @@ app.post("/articles/:id", function(req, res) {
     })
     .then(function(dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// route for deleting an article
+app.delete("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that updates the matching one in our db...
+  db.Article.remove({ _id: req.params.id })
+
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
